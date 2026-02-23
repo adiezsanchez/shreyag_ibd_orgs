24e5df2235429161dda556040d2d56b38fad7d19
adiezsanchez <adiez.biotech@gmail.com>
2026-02-23 10:58:38 +0100

    refactor: remove unused function and warnings

--- src/organoid_analysis/utils.py
+++ src/organoid_analysis/utils.py
@@ -7,7 +7,6 @@ from skimage.measure import regionprops_table
 from skimage.segmentation import relabel_sequential
 from skimage.morphology import remove_small_objects
 import pyclesperanto_prototype as cle
-import warnings
 
 cle.select_device("RTX")
 
@@ -98,102 +97,6 @@ def simulate_cytoplasm(nuclei_labels, dilation_radius=2, erosion_radius=0):
     cytoplasm[nuclei_mask] = 0
 
     return cytoplasm
-
-def simulate_cytoplasm_chunked_3d(nuclei_labels, dilation_radius=2, erosion_radius=0, chunk_size=(1, 1024, 1024)):
-    """
-    Simulates cytoplasm expansion around labeled nuclei in a 3D volume using chunked processing.
-    Nuclei region is masked out generating a hollow sphere around it.
-
-    Parameters:
-    nuclei_labels (ndarray): 3D array of labeled nuclei.
-    dilation_radius (int, optional): Radius for dilation of the nuclei. Default is 2.
-    erosion_radius (int, optional): Radius for erosion of the nuclei. Default is 0.
-    chunk_size (tuple, optional): Size of the chunks to process (z, y, x). Default is (1, 1024, 1024).
-
-    Returns:
-    ndarray: 3D array representing the simulated cytoplasm with nuclei regions removed. The values in the returned
-             array indicate the cytoplasm regions after dilation, with zero values corresponding to the original 
-             nuclei positions, ensuring no overlap.
-    """
-    cytoplasm = np.zeros_like(nuclei_labels)
-    
-    # Process the data in chunks to optimize memory usage and allow processing of large datasets
-    for z in range(0, nuclei_labels.shape[0], chunk_size[0]):
-        for y in range(0, nuclei_labels.shape[1], chunk_size[1]):
-            for x in range(0, nuclei_labels.shape[2], chunk_size[2]):
-                chunk = nuclei_labels[z:z+chunk_size[0], y:y+chunk_size[1], x:x+chunk_size[2]]
-                
-                # Apply erosion only if the radius is greater than or equal to 1 to avoid unnecessary processing
-                if erosion_radius >= 1:
-                    eroded_chunk = cle.erode_labels(chunk, radius=erosion_radius)
-                    eroded_chunk = cle.pull(eroded_chunk)
-                    chunk = eroded_chunk
-
-                cyto_chunk = cle.dilate_labels(chunk, radius=dilation_radius)
-                cyto_chunk = cle.pull(cyto_chunk)
-
-                # Create a binary mask of the nuclei
-                chunk_mask = chunk > 0
-                # Set the corresponding values in the cyto_chunk array to zero
-                cyto_chunk[chunk_mask] = 0
-
-                cytoplasm[z:z+chunk_size[0], y:y+chunk_size[1], x:x+chunk_size[2]] = cyto_chunk
-
-    # Compare unique labels directly as numpy arrays
-    nuclei_labels_unique = np.unique(nuclei_labels)
-    cytoplasm_labels_unique = np.unique(cytoplasm)
-
-    if not np.array_equal(nuclei_labels_unique, cytoplasm_labels_unique):
-
-        # Identify nuclei labels that are missing in the cytoplasm
-        # This can occur when a nucleus is completely surrounded by other labels,
-        # preventing it from expanding during the dilation process.
-        # In such cases, the dilated nucleus remains the same size as the original.
-        # When the original nucleus is subtracted from the dilated result,
-        # it disappears entirely, since both have the same size and shape.
-        missing_in_cytoplasm = np.setdiff1d(nuclei_labels_unique, cytoplasm_labels_unique)
-
-        # Calculate the percentage of lost labels and determine if the loss is significant (>1%).
-        loss_percentage = round((len(missing_in_cytoplasm) / len(nuclei_labels_unique)) * 100, 2)
-
-        if loss_percentage > 1:
-            warnings.warn(
-                f"\nMismatch in label sets! "
-                f"Nuclei labels: {len(nuclei_labels_unique)}, Cytoplasm labels: {len(cytoplasm_labels_unique)}.\n"
-                f"Missing labels in Cytoplasm: {missing_in_cytoplasm[:10]}{'...' if len(missing_in_cytoplasm) > 10 else ''}\n"
-                f"{loss_percentage}% of cells lost during nuclei subtraction operation.\n"
-                f"If this percentage is too high, consider using 'cell' instead of 'cytoplasm' for marker placement. "
-                f"The nuclei may be too densely packed."
-            )
-
-    return cytoplasm
-
-def simulate_cell_chunked_3d(nuclei_labels, dilation_radius=2, erosion_radius=0, chunk_size=(1, 1024, 1024)):
-    cell = np.zeros_like(nuclei_labels)
-    
-    for z in range(0, nuclei_labels.shape[0], chunk_size[0]):
-        for y in range(0, nuclei_labels.shape[1], chunk_size[1]):
-            for x in range(0, nuclei_labels.shape[2], chunk_size[2]):
-                chunk = nuclei_labels[z:z+chunk_size[0], y:y+chunk_size[1], x:x+chunk_size[2]]
-                
-                if erosion_radius >= 1:
-                    eroded_chunk = cle.erode_labels(chunk, radius=erosion_radius)
-                    eroded_chunk = cle.pull(eroded_chunk)
-                    chunk = eroded_chunk
-
-                cell_chunk = cle.dilate_labels(chunk, radius=dilation_radius)
-                cell_chunk = cle.pull(cell_chunk)
-
-                cell[z:z+chunk_size[0], y:y+chunk_size[1], x:x+chunk_size[2]] = cell_chunk
-
-    # Compare unique labels directly as numpy arrays
-    nuclei_labels_unique = np.unique(nuclei_labels)
-    cell_labels_unique = np.unique(cell)
-
-    if not np.array_equal(nuclei_labels_unique, cell_labels_unique):
-        warnings.warn(f"Mismatch in label sets! Nuclei labels: {len(nuclei_labels_unique)}, Cell labels: {len(cell_labels_unique)}")
-    
-    return cell
 
 def segment_organoids_from_cp_labels(cytoplasm_labels):
     
